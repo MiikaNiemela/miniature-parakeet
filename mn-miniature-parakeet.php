450e15d340cc5e4b529cd7cd46c4013d9e6873ce
Miika Niemelä <miika@niemelat.com>
2018-01-19 06:19:22 +0200

    added headers

--- mn-miniature-parakeet.php
+++ mn-miniature-parakeet.php
@@ -1,3 +1,14 @@
 <?php
-
+/*
+Plugin Name:  Miniature Parakeet
+Plugin URI:   https://github.com/MiikaNiemela/miniature-parakeet/
+Description:  Ajax tests
+Version:      20180119
+Author:       Miika Niemelä
+Author URI:   https://github.com/MiikaNiemela/
+License:      GPL3
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  wporg
+Domain Path:  /
+*/
 ?>
